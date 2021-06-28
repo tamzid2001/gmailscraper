d0f9005f3f84854357c5bb64ee0dbebc113b134e
Tamzid Ullah <tamzid257@gmail.com>
2021-06-28 14:01:51 -0500

    -new

--- SoShUser.js
+++ SoShUser.js
@@ -1,4 +1,30 @@
-const parser = new DOMParser();
+const fetch = require('node-fetch');
+const {google} = require('googleapis');
+var Crawler = require("crawler");
+const htmlparser2 = require("htmlparser2");
+
+async function t(b){
+  var y = "";
+const parser = new htmlparser2.Parser({
+    ontext(text) {
+        /*
+         * Fires whenever a section of text was processed.
+         *
+         * Note that this can fire at any point within text and you might
+         * have to stich together multiple pieces.
+         */
+        y = y + " " + text
+    },
+    onend(){
+      return y;
+    }
+  });
+  parser.write(b);
+  parser.end();
+}
+
+const commerce_api_key = "fd9acead3484e90a065057c2696ef798";
+const commerce_secret_key = "2541b85dd9d09d094cae96e64e108253591395f6";
 
 class User {
     constructor(name, posts) {
@@ -34,8 +60,9 @@ class User {
   }
   
   class Body {
-    constructor(bod) {
+    constructor(bod, cuid) {
         this.bod = bod;
+        this.cuid = cuid;
       }
   
     get post(){
@@ -46,19 +73,48 @@ class User {
       return this.isPurchase();
     }
   
-    isPurchase(){
+    async isPurchase(){
+      var is = false;
       const regex = /((recei(pts?|ved)|confirm(ation|ed)|purchas(ed?|e)|total|order))/gi;
-      if(this.bod.matchAll(regex) != []){
-        return true;
+      //const doc3 = parser.parseFromString(this.bod, "text/html");
+      const y = await t(this.bod);
+      const website = /[\w]+[\.](com)/gim
+      const rt = /^(\D.*\S)\s+\$([0-9.]+)\)?\s*/gim;
+      //const name = website.replace(".com", "");
+      const clean = /\r?\n|\r/g;
+      var foundcom = [...y.matchAll(website)];
+      if([...y.matchAll(regex)].length != 0 ){
+        if(foundcom.length != 0){
+          foundcom.forEach((e)=>{
+            if(e[0] != "gmail.com" && e[0] != "google.com" && e[0] != undefined){
+              is = true;
+            }
+          })
+          var price = [...y.matchAll(rt)];
+          if(price.length != 0){
+            is = true;
+          } else {
+            is = false;
+          }
+          return is;
+        } else {
+          return false;
+        }
       } else {
         return false;
       }
     }
   
-    createPost(){
-        const doc3 = parser.parseFromString(this.bod, "text/html");
+    async createPost(){
+      var im = "";
+      var affiliate_link;
+      var e;
+      var w;
+      var c;
+      var l = "";
+        //const doc3 = parser.parseFromString(this.bod, "text/html");
         const nq = /(.+\s+)((qty|Qty|Quantity|quantity)\W+\s+)+([0-9]+)/gi;
-        const y = doc3.body.firstChild.textContent;
+        const y = await t(this.bod);
         const regex = /(Total )[\$][\d]+[\.][\d]{2}/g;
         const rt = /^(\D.*\S)\s+\$([0-9.]+)\)?\s*/gim;
         const website = /[\w]+[\.](com)/gim
@@ -67,9 +123,13 @@ class User {
         const clean = /\r?\n|\r/g;
         var foundcom = [...y.matchAll(website)];
         if(foundcom.length != 0){
-          var namec = foundcom[0][0].replace(".com", "");
-          var w = foundcom[0][0].replace(clean, "");
-          var c = namec.replace(clean, "");
+          foundcom.forEach((e)=>{
+            if(e[0] != "gmail.com" && e[0] != "google.com" && e[0] != undefined){
+              var namec = e[0].replace(".com", "");
+              w = e[0].replace(clean, "");
+              c = namec.replace(clean, "");
+            } 
+          })
         } else {
           namec = "";
           w = "";
@@ -88,12 +148,112 @@ class User {
           const qu = quan[0][0].replace(clean, "");
           i = qu;
         } else if(price.length != 0){
-          i = price[0][0].replace(clean,"")
+          i = price[0][2].replace(clean,"")
         } else {
           i = "";
         }
-        const l = "";
-        const im = "";
+
+    const checkD = () => {
+
+      if(w != ""){
+      
+    fetch(`https://publishers.viglink.com/api/merchant/search?domain=${w}`, {
+        method: 'get',
+        headers: { 
+          'Access-Control-Allow-Headers': '*',
+          'Access-Control-Allow-Origin': 'https://soshwrld.com',
+          'Vary': 'Origin',
+          'Authorization': `${commerce_secret_key}`
+       }
+    })
+    .then(res => res.json())
+    .then((json) => {
+      //check domain ----------------------------------------------------> 
+      console.log(json)
+      json.merchants.forEach((a)=>{
+        a.domains.forEach((v)=>{
+          if(v === w){
+            return true;
+          }
+        });
+      })
+      return false;
+    })
+    .catch((err)=>{
+      //fake domain - fake purchase
+      console.log(err)
+    });
+  }
+}
+
+if(p != ""){
+
+  await fetch(`https://www.googleapis.com/customsearch/v1?key=AIzaSyAUSNeT750wI-oDmpQm4AZcsYmZfc-ShSU&cx=430def6a414e30530&q=\"${p}\":${w}`)
+  .then(async (res)=>{return await res.json()})
+  .then((res) => {
+    l = res.items[0].link;
+    im = res.items[0].pagemap.cse_thumbnail[0].src;
+    console.log(l)
+    console.log(im)
+    console.log(res.items[0].link, res.items[0].pagemap.cse_thumbnail[0].src)
+    return new Post(w, c, i, p, l, im);
+  })
+  .catch((err) => {
+  console.log(err);
+  });
+
+    // fetch(`https://api.viglink.com/api/link?format=json&out=${l}&key=${commerce_api_key}`)
+    //     .then(res => res.json())
+    //     .then((json) => {
+    //       if(json.affiliable){
+    //         affiliate_link = json.optimized; //------------------------------------------------>
+    //         e = parseFloat(json.eepc) * 100; //convert to %
+    //       } else {
+    //         affiliate_link = "";
+    //         e = "";
+    //       }
+    //   console.log(json)
+    // }).catch((err)=>{
+    //   //failed to get aff
+    //         affiliate_link = "";
+    //         e = "";
+    //   console.log(err);
+    // });
+
+  }
+
+
+
+    // gapi.client.search.cse.list({
+    //   "c2coff": "1",
+    //   "cx": "430def6a414e30530",
+    //   "q": `\"${itemName}\":${domainName}`
+    // })
+    //     .then(function(response) {
+    //             // Handle the results here (response.result has the parsed body).
+    //             console.log("Response", response);
+    //             console.log(response.result.items[0].link);
+    //             const l = response.result.items[0].link; // product link
+    //           },
+    //           function(err) { console.error("Execute error", err); });
+
+    //grab all images from product url
+            //   const craw = new Crawler({
+            //     callback: function(error, res, done) {
+            //         if (error) {
+            //             console.log({error})
+            //         } else {
+            //             const images = res.$('img')
+            //             images.each(index => {
+            //                 // here you can save the file or save them in an array to download them later
+            //                 productImages.push({src: images[index].attribs.src, alt: images[index].attribs.alt})
+                            
+            //             })
+            //         }
+            //     }
+            // })
+            
+            // craw.queue(l)
         return new Post(w, c, i, p, l, im);
     }
   }
--- index.js
+++ index.js
@@ -15,7 +15,19 @@ const fetch = require('node-fetch');
 const envFilePath = path.resolve(__dirname, './.env');
 const env = require("dotenv").config({ path: envFilePath });
 var base64 = require('js-base64').Base64;
+const stream = require('getstream'); 
+const htmlparser2 = require("htmlparser2");
 
+app.use(bodyParser.json());
+app.use(cookieParser());
+ 
+// instantiate a new client (server side) 
+const streamClient = stream.connect( 
+  't6d6x66485xc', 
+  '3q9mx28vhd32ge8bef7s7k4fqxcnzf7cv3nwh7ana5ypwjvsqq5hwqhqqz96ft2d' 
+); 
+ 
+const apis = google.getSupportedAPIs();
 var serviceAccount = require("./soshwrldinc-firebase-adminsdk-ky99h-dc649f9567.json");
 
 admin.initializeApp({
@@ -23,21 +35,174 @@ admin.initializeApp({
 });
 
 const db = admin.firestore();
-const userbase = db.collection('users');
+const userbase = db.collection('UserBase');
 
-import {User, Body, Post} from './SoShUser.js'
+const stripe = require('stripe')('------------------------');
+
+async function createPayout() {
+  const account = await stripe.accounts.create({
+    type: 'express',
+  });
+
+  const accountLinks = await stripe.accountLinks.create({
+    account: account.id,
+    refresh_url: 'https://example.com/reauth',
+    return_url: 'https://example.com/return',
+    type: 'account_onboarding',
+  });
+
+  return {stripe_account_id: account.id, stripe_account_link: accountLinks}
+}
+
+const {User, Body, Post} = require('./SoShUser.js');
 // const subscriptionName = 'YOUR_SUBSCRIPTION_NAME';
 // const timeout = 60;
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/functions'));
+
+const csrfMiddleware = csrf({ cookie: true });
+app.use(csrfMiddleware);
+
+app.all("*", (req, res, next) => {
+  res.cookie("XSRF-TOKEN", req.csrfToken());
+  next();
+});
+
+var decode = function(input) {
+  // Replace non-url compatible chars with base64 standard chars
+  input = input.replace(/-/g, '+');
+  input = input.replace(/_/g, '/');
+
+  // Pad out with standard base64 required padding characters
+  var pad = input.length % 4;
+  if(pad) {
+    if(pad === 1) {
+      throw new Error('InvalidLengthError: Input base64url string is the wrong length to determine padding');
+    }
+    input += new Array(5-pad).join('=');
+  }
+
+  return input
+}
+
+app.get("/getStreamUserToken", (req, res) => {
+  const sessionCookie = req.cookies.__session || "";
+  admin.auth().verifySessionCookie(sessionCookie, true /** checkRevoked */)
+    .then(async (x) => {
+
+      const userToken = client.createUserToken(x.uid);
+      res.status(200).send(JSON.stringify({stream_user_token: userToken}))
+
+    });
+})
+
+app.get("/revenueReport", (req, res) => {
+  var cuid;
+  var u = userbase.doc(x.uid).get().then((doc)=>{
+    var d = doc.data();
+    cuid = d.cuid;
+  })
+  
+  fetch(`https://viglink.io/transactions?cuid=${cuid}&sumByField=publisherRevenue`).then((r)=>{
+    res.status(200).send(JSON.stringify({totalRevenue: r.buckets[0].sumValue}))
+  })
+
+})
+
+app.post("/sendPayout", (req,res) => {
+  const sessionCookie = req.cookies.__session || "";
+  admin.auth().verifySessionCookie(sessionCookie, true /** checkRevoked */)
+    .then(async (x) => {
+      var wa;
+      var aid;
+      userbase.doc(x.uid).get().then((d)=>{
+        var data = d.data();
+        data.forEach((element)=>{
+          wa =  element.withdrawal_amount;
+          aid = element.stripe_account_id;
+        })
+      })
+      
+  const transfer = await stripe.transfers.create({
+    amount: wa,
+    currency: "usd",
+    destination: aid,
+  });
+
+}).catch((err)=>{
+  res.status(404).send(err);
+})
+
+})
+
+
 
 app.post('/access', (req,res) => {
-  var t = req.body.access_token;
-  userbase.doc(uid).update({access_token: t}).then(()=>{
-    res.status(200).send();
-  });
+  const sessionCookie = req.cookies.__session || "";
+  var t = req.body.gmail_access_token;
+  admin.auth().verifySessionCookie(sessionCookie, true /** checkRevoked */)
+    .then(async (x) => {
+      if(user.id === x.uid){
+
+        userbase.doc(x.uid).update({gmail_access_token: t}).then(()=>{
+          res.status(200).send(p);
+        })
+    }})
+    .catch((error) => {
+      res.status(404).send('User is not logged in. ', err);
+    });
 
 
 })
+
+app.post('/createuser', (req,res) => {
+  const sessionCookie = req.cookies.__session || "";
+  var p = req.body.phone_number;
+  var f = req.body.first_name;
+  var l = req.body.last_name;
+  var un = req.body.user_name;
+  admin.auth().verifySessionCookie(sessionCookie, true /** checkRevoked */)
+    .then(async (x) => {
+      if(user.id === x.uid){
+
+        userbase.doc(x.uid).set({
+          phone_number: p, 
+          first_name: f, 
+          last_name: l, 
+          user_name: un, 
+          gmail_access_token: "", 
+          gmail_refersh_token: "", 
+          post: [], 
+          profile_pic: "", 
+          pending_amount: "", 
+          withdrawal_amount: "",
+          bio: "",
+          saved_posts: [],
+          cuid: ""
+        }).then(()=>{
+          res.status(200).send(p);
+        })
+    }
+  })
+    .catch((error) => {
+      res.status(404).send('User is not logged in. ', err);
+    });
+  });
+
+  app.get("/posts", (req, res) => {
+    const sessionCookie = req.cookies.__session || "";
+    //watch gmail account
+    //send post object
+    admin.auth().verifySessionCookie(sessionCookie, true /** checkRevoked */)
+    .then(async (x) => {
+      var d = userbase.doc(x.uid).get((doc)=>{
+        var data = doc.data();
+        authorize((auth) => {
+          getPosts(data.access_token, auth)
+        });
+        res.status(200).send(JSON.stringify({posts: u.userPosts}))
+      })
+    });
+  })
 
   //////////////////////////////////////////////////////////////////////////////////////
 // Imports the Google Cloud client library
@@ -52,8 +217,8 @@ const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly', "https://www.g
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
-const TOKEN_PATH = 'token.json';
-
+//const TOKEN_PATH = 'token.json';
+/*
 // Load client secrets from a local file.
 fs.readFile('credentials.json', (err, content) => {
     if (err) return console.log('Error loading client secret file:', err);
@@ -62,28 +227,44 @@ fs.readFile('credentials.json', (err, content) => {
         listUnreadMsgs(auth), watchMyLabel(auth)
     });
 });
-
+*/
 /**
  * Create an OAuth2 client with the given credentials, and then execute the
  * given callback function.
  * @param {Object} credentials The authorization client credentials.
  * @param {function} callback The callback to call with the authorized client.
  */
-function authorize(credentials, callback) {
-    const {
-        client_secret,
-        client_id,
-        redirect_uris
-    } = credentials.installed;
+function authorize(callback) {
+
+  const sessionCookie = req.cookies.__session || "";
+  const client_id = "342162757131-r601ab2kkk865gagvk8a83knr544p79q.apps.googleusercontent.com";
+  const client_secret = "hFZxZ7R0dJS14P691Tvlm2vf";
+  const redirect_uris = "https://soshwrldinc.firebaseapp.com/__/auth/handler";
     const oAuth2Client = new google.auth.OAuth2(
-        client_id, client_secret, redirect_uris[0]);
+        client_id, client_secret, redirect_uris);
 
     // Check if we have previously stored a token.
-    fs.readFile(TOKEN_PATH, (err, token) => {
-        if (err) return getNewToken(oAuth2Client, callback);
-        oAuth2Client.setCredentials(JSON.parse(token));
-        callback(oAuth2Client);
+    
+    admin.auth().verifySessionCookie(sessionCookie, true /** checkRevoked */)
+    .then(async (x) => {
+      if(user.id === x.uid){
+
+        userbase.doc(x.uid).get().then((documentSnapshot)=>{
+          documentSnapshot.data().forEach((element) => {
+            //getNewToken(oAuth2Client, callback)
+            oAuth2Client.setCredentials(element.access_token);
+            callback(oAuth2Client);
+          })
+        })
+    }
+  })
+    .catch((error) => {
+      res.status(404).send('User is not logged in. ', error);
     });
+        // if (err) return getNewToken(oAuth2Client, callback);
+        // oAuth2Client.setCredentials(JSON.parse(token));
+        // callback(oAuth2Client);
+    
 }
 
 /**
@@ -107,7 +288,7 @@ function getNewToken(oAuth2Client, callback) {
         oAuth2Client.getToken(code, (err, token) => {
             if (err) return console.error('Error retrieving access token', err);
             oAuth2Client.setCredentials(token);
-            db.collection('users').doc(auth.uid).update({accesstoken: token});
+            //db.collection('users').doc(auth.uid).update({accesstoken: token}); //==========================>
             // Store the token to disk for later program executions
             fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
                 if (err) return console.error(err);
@@ -172,36 +353,53 @@ async function watchMyLabel(auth) {
 
 //client id 342162757131-ltcjavnkgijv5f96e7jbhqbtd0m1jg2g.apps.googleusercontent.com
 //client secret xLa_tAN5z9GCCUIGorIqopBb
-function listenForMessages() {
-    const subscriptionName = "projects/soshwrldinc/topics/updates"
+function listenForMessages(a, auth) {
+    const subscriptionName = "projects/soshwrldinc/subscriptions/updates-sub"
   // References an existing subscription
   const subscription = pubSubClient.subscription(subscriptionName);
 
   // Create an event handler to handle messages
   let messageCount = 0;
-
+  var timeout = 60;
   const messageHandler = message => {
     const po = [];
     const gmail = google.gmail({version: 'v1', auth});
     const name = gmail.users.getProfile.name
     gmail.users.messages.list({
         userId: 'me',
+        access_token: a
       }, (err, res) => {
         if (err) return console.log('The API returned an error: ' + err);
           const labels = res.data.messages
           if(labels.length){
             labels.forEach((l) => {
-              console.log(l.id);
-              console.log(res.data.messages.values());
-              const b = new Body(l.raw)
-              if(b.isPost()){
-                po.push(b.post());
-              }
+              gmail.users.messages.get({
+                "userId": "me",
+                "id": l.id,
+                "access_token": a
+              }).then(async (res)=>{
+                //console.log(res);
+                var json = res.result.payload.parts
+                      for(var i = 0; i < json.length; i++) {
+                        var obj = json[i];
+                        if(obj.mimeType == "text/html"){
+                          var body = decode(obj.body.data);
+                          body = window.atob(body);
+                          const post = new Body(body);
+                          if(post.isPost){
+                            var pox = await post.post;
+                            po.push(pox.data)
+                          }
+                        }
+                      }
+              }).catch((err)=>{
+                console.log(err);
+              })
             })
           };
           const user = new User(name, po);
         
-        db.collection('users').doc(auth.uid).update({posts: po, name: name})
+          //db.collection('users').doc(x.uid).update({posts: po, name: name})
         
           
           return user;
@@ -224,11 +422,204 @@ function listenForMessages() {
   }, timeout * 1000);
 }
 
-listenForMessages();
+function recursiveLabels(page){
+  gmail.users.messages.list({
+    userId: 'me',
+    nextPageToken: page
+  }, (err, res) => {
+    if (err) return console.log('The API returned an error: ' + err);
+      const labels = res.data.messages
+  })
 
-app.listen(5000, () => console.log(`Node server listening at https://gurugains-fac88.web.app:${5000}/`));
+}
+
+function recur(a, auth, page, h) {
+  const po = h;
+  const gmail = google.gmail({version: 'v1', auth});
+  const name = gmail.users.getProfile.name
+  if(page){
+  gmail.users.messages.list({
+      userId: 'me',
+      access_token: a,
+      pageToken: page
+    }, (err, res) => {
+      if (err) return console.log('The API returned an error: ' + err);
+        const labels = res.data.messages
+        if(labels.length){
+          labels.forEach((l) => {
+            gmail.users.messages.get({
+              "userId": "me",
+              "id": l.id,
+              "access_token": a //---------------------->
+            }).then(async (res)=>{
+              //console.log(res);
+              var json = res.result.payload.parts
+                    for(var i = 0; i < json.length; i++) {
+                      var obj = json[i];
+                      if(obj.mimeType == "text/html"){
+                        var body = decode(obj.body.data);
+                        body = window.atob(body);
+                        const post = new Body(body);
+                        if(post.isPost){
+                          var pox = await post.post;
+                          po.push(pox.data)
+                        }
+                      }
+                    }
+            }).catch((err)=>{
+              console.log(err);
+            })
+          })
+        };
+        const user = new User(name, po);
+      
+        //db.collection('users').doc(x.uid).update({posts: po, name: name})
+      
+        
+        return po;
+  })
+} else {
+  return po;
+}
+
+}
+
+function getPosts(a, auth) {
+  const po = [];
+  var pageToken;
+  const gmail = google.gmail({version: 'v1', auth});
+  const name = gmail.users.getProfile.name
+  gmail.users.messages.list({
+      userId: 'me',
+      access_token: a
+    }, (err, res) => {
+      if (err) return console.log('The API returned an error: ' + err);
+        const labels = res.data.messages
+        pageToken = res.nextPageToken;
+        if(labels.length){
+          labels.forEach((l) => {
+            gmail.users.messages.get({
+              "userId": "me",
+              "id": l.id,
+              "access_token": a //---------------------->
+            }).then(async (res)=>{
+              //console.log(res);
+              var json = res.result.payload.parts
+                    for(var i = 0; i < json.length; i++) {
+                      var obj = json[i];
+                      if(obj.mimeType == "text/html"){
+                        var body = decode(obj.body.data);
+                        body = window.atob(body);
+                        const post = new Body(body);
+                        if(post.isPost){
+                          var pox = await post.post;
+                          po.push(pox.data)
+                        }
+                      }
+                    }
+            }).catch((err)=>{
+              console.log(err);
+            })
+          })
+        };
+        //const user = new User(name, po);
+      
+        //db.collection('users').doc(x.uid).update({posts: po, name: name})
+      
+        
+        
+  });
+  recur(a, auth, pageToken, po)
+  //return po;
+
+
+}
+
+//listenForMessages();
+
+app.post("/sessionLogin", (req, res) => {
+  const idToken = req.body.idToken.toString();
+
+  const expiresIn = 60 * 60 * 24 * 5 * 1000;
+  admin.auth().verifyIdToken(idToken).then(function(decodedClaims) {
+    // In this case, we are enforcing that the user signed in in the last 5 minutes.
+    if (new Date().getTime() / 1000 - decodedClaims.auth_time < 5 * 60) {
+      return admin.auth().createSessionCookie(idToken, {expiresIn: expiresIn});
+    }
+    throw new Error('UNAUTHORIZED REQUEST!');
+  }).then(function(sessionCookie) {
+    // Note httpOnly cookie will not be accessible from javascript.
+    // secure flag should be set to true in production.
+    const options = {maxAge: expiresIn, secure: false /** to test in localhost */};
+    res.cookie('__session', sessionCookie, options);
+    res.end(JSON.stringify({status: 'success'}));
+  })
+  .catch(function(error) {
+    res.status(401).send('UNAUTHORIZED REQUEST!');
+  });
+  // admin
+  //   .auth()
+  //   .createSessionCookie(idToken, { expiresIn })
+  //   .then(async (sessionCookie) => {
+  //       const options = { maxAge: expiresIn, httpOnly: true };
+  //       res.cookie("session", sessionCookie, options);
+  //       res.end(JSON.stringify({ status: "success" }));
+  //     })    
+  //     .catch((error) => {
+  //       res.status(401).send("UNAUTHORIZED REQUEST!");
+  //     })
+    
+});
+
+app.get("/sessionLogout", (req, res) => {
+  res.clearCookie("__session");
+  res.redirect("/app/login");
+});
+
+app.listen(5000, () => console.log(`Node server listening at https://.web.app:${5000}/`));
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.app = functions.https.onRequest(app);
+
+exports.getStreamUserToken = functions.https.onCall((data, context) => {
+  const firebaseIDToken = data.firebaseIDToken;
+  return new Promise((resolve, reject) => {
+    admin
+  .auth()
+  .verifyIdToken(firebaseIDToken)
+  .then((decodedToken) => {
+    const uid = decodedToken.uid;
+
+    const streamUserToken = streamClient.createUserToken(uid);
+    resolve({"token":streamUserToken});
+    return;
+  })
+  .catch((err) => {
+    reject(err);
+    return;
+  });
+
+});
+});
+
+exports.sendAuthCode = functions.https.onCall((data, context) => {
+  const code = data.auth_code;
+  const uid = data.uid;
+  const client_id = "342162757131-r601ab2kkk865gagvk8a83knr544p79q.apps.googleusercontent.com";
+  const client_secret = "hFZxZ7R0dJS14P691Tvlm2vf";
+  const redirect_uris = "https://soshwrld.com";
+  const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris);
+  return new Promise((resolve, reject) => {
+
+    oAuth2Client.getToken(code).then((token)=>{
+      //oAuth2Client.setCredentials(token);
+      
+      userbase.doc(uid).update({accessToken: token})
+      resolve({"accessToken": token})
+    }).catch((err)=>{
+      reject(err);
+    })
+});
+});
